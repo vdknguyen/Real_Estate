91814103167cd8d00f8ce9218194629536407145
vdknguyen <vdknguyenx@gmail.com>
2016-10-23 12:35:27 -0700

    Added rental option
    Matched BQ

--- house_master.py
+++ house_master.py
@@ -24,11 +24,34 @@ class House():
         self.inputs = inputs
 
     
-    def calculate_montly_payment(self,n,L,monthly_interest):
+    def amortization(self,n,interest,marginal_tax_rate,price,offer,down_payment,yearly_house_appreciation):
+        L = offer * (1.0 - down_payment) # loan amount
+        monthly_interest = interest/12.0 # monthly interest rate as a percent/100
         I = monthly_interest
         numerator = L*(1 + I)**n
         denomenator = -1*(1 - (1 + I)**n) / I
-        return (numerator/denomenator)
+        p = numerator/denomenator # monthly payment
+        # Amortization
+        mortgage = pd.DataFrame()
+        loan = L
+        month_list = range(1,n+1)
+        for month in month_list: # simulate monthly payment
+            mortgage_month = pd.DataFrame()
+            mortgage_month['Month'] = [month]
+            mortgage_month['Loan_Before_Payment'] = loan
+            loan = loan * (1+ monthly_interest) - p
+            mortgage_month['Loan_After_Payment'] = loan
+            mortgage_month['Equity_Gain'] = mortgage_month['Loan_Before_Payment'] - mortgage_month['Loan_After_Payment']
+            mortgage = pd.concat([mortgage,mortgage_month])
+        mortgage['Interest_Paid'] = p - mortgage['Equity_Gain']
+        mortgage['Tax_Savings'] = mortgage['Interest_Paid'] * marginal_tax_rate
+        mortgage['Cumulative_Tax_Savings'] = mortgage['Tax_Savings'].cumsum()
+        mortgage['Year'] = np.ceil(mortgage['Month']/12.0)
+        mortgage['Cumulative_Equity_Gain'] = mortgage['Equity_Gain'].cumsum()
+        mortgage['Cumulative_Interest_Paid'] = mortgage['Interest_Paid'].cumsum()
+        mortgage['Total_Equity_With_Appreciation'] = (mortgage['Cumulative_Equity_Gain']*(price/offer) + down_payment * price) * (1 + yearly_house_appreciation)**mortgage['Year']
+        mortgage.set_index([range(len(mortgage))], inplace = True)
+        return mortgage,p
 
     def calculate_cost(self,inputs):
 
@@ -65,38 +88,12 @@ class House():
         investment_return = inputs['investment_return']
         opportunity_rent = inputs['opportunity_rent']
 
-        # For calculations: do not edit
+        # Amortization
         n = time * 12 #number of months for the loan
-        L = offer * (1.0 - down_payment) # loan amount
-        monthly_interest = interest/12.0 # monthly interest rate as a percent/100
-        p = self.calculate_montly_payment(n,L,monthly_interest) # monthly payment
+        mortgage,p = self.amortization(n,interest,marginal_tax_rate,price,offer,down_payment,yearly_house_appreciation)
+    
 
-
-
-
-        mortgage = pd.DataFrame()
-        loan = L
-        month_list = range(1,n+1)
-        for month in month_list: # simulate monthly payment
-            mortgage_month = pd.DataFrame()
-            mortgage_month['Month'] = [month]
-            mortgage_month['Loan_Before_Payment'] = loan
-            loan = loan * (1+ monthly_interest) - p
-            mortgage_month['Loan_After_Payment'] = loan
-            mortgage_month['Equity_Gain'] = mortgage_month['Loan_Before_Payment'] - mortgage_month['Loan_After_Payment']
-            mortgage = pd.concat([mortgage,mortgage_month])
-        mortgage['Interest_Paid'] = p - mortgage['Equity_Gain']
-        mortgage['Tax_Savings'] = mortgage['Interest_Paid'] * marginal_tax_rate
-        mortgage['Cumulative_Tax_Savings'] = mortgage['Tax_Savings'].cumsum()
-        mortgage['Year'] = np.ceil(mortgage['Month']/12.0)
-        mortgage['Cumulative_Equity_Gain'] = mortgage['Equity_Gain'].cumsum()
-        mortgage['Cumulative_Interest_Paid'] = mortgage['Interest_Paid'].cumsum()
-        mortgage['Total_Equity_With_Appreciation'] = (mortgage['Cumulative_Equity_Gain']*(price/offer) + down_payment * price) * (1 + yearly_house_appreciation)**mortgage['Year']
-        mortgage.set_index([range(len(mortgage))], inplace = True)
-
-
-
-        # # Yearly Info:
+        # Yearly Info:
 
         yearly_mortgage = mortgage[mortgage.Month%12 == 0] # Gives end of the year
         yearly_mortgage = yearly_mortgage[['Year','Cumulative_Interest_Paid','Total_Equity_With_Appreciation','Cumulative_Tax_Savings']]
@@ -108,11 +105,11 @@ class House():
         yearly_mortgage['Property_Tax'] = property_tax * yearly_mortgage['House_Value']
         yearly_mortgage['Cumulative_Property_Tax'] = yearly_mortgage['Property_Tax'].cumsum()
         yearly_mortgage['Tax_Savings'] = yearly_mortgage['Cumulative_Tax_Savings']
-        yearly_mortgage['Tax_Savings'].iloc[1:len(yearly_mortgage)] = np.diff(yearly_mortgage['Cumulative_Tax_Savings'])
-        yearly_mortgage['Tax_Savings'] += marginal_tax_rate * yearly_mortgage['Property_Tax']
+        yearly_mortgage['Tax_Savings'].iloc[1:len(yearly_mortgage)] = np.diff(yearly_mortgage['Cumulative_Tax_Savings']) # Yearly tax deduction from interest
+        yearly_mortgage['Tax_Savings'] += marginal_tax_rate * yearly_mortgage['Property_Tax'] # yearly tax deduction from property tax
         yearly_mortgage['Expected_Tax_Savings'] = (expected_deduction + expected_deduction_yearly_increase * mortgage['Year']) * (marginal_tax_rate) # Tax savings from standard dedecution
-        yearly_mortgage['Effective_Tax_Savings'] = yearly_mortgage['Tax_Savings'] - yearly_mortgage['Expected_Tax_Savings'] # Only count if savings above what you would get from standard deduction
-        yearly_mortgage['Effective_Tax_Savings'] = yearly_mortgage['Effective_Tax_Savings'].apply(lambda x: x if x > 0 else 0)
+        yearly_mortgage['Effective_Tax_Savings'] = yearly_mortgage['Tax_Savings'] - yearly_mortgage['Expected_Tax_Savings'] # Difference between interest+property tax deduction and standard deduction
+        yearly_mortgage['Effective_Tax_Savings'] = yearly_mortgage['Effective_Tax_Savings'].apply(lambda x: x if x > 0 else 0) # Only count if savings above what you would get from standard deduction
         yearly_mortgage['Cumulative_Tax_Savings'] = yearly_mortgage['Effective_Tax_Savings'].cumsum()
 
         # Total Costs
@@ -125,7 +122,7 @@ class House():
         yearly_mortgage['Total_Cost'] = yearly_mortgage['Cumulative_Property_Tax'] + yearly_mortgage['Cumulative_Interest_Paid']+ yearly_mortgage['Cumulative_HOA']+ yearly_mortgage['Cumulative_Homeowner_Insurance']+ yearly_mortgage['Cumulative_Maintenance'] + offer * closing_buy + yearly_mortgage['House_Value'] * closing_sell
 
         # Rental income
-        yearly_mortgage['Rent_Income'] = ((1-marginal_tax_rate) * (1 - percent_vacancy) * 12 * monthly_rent * rent_percent ) * ((1 + rent_yearly_increase) ** yearly_mortgage['Year'])
+        yearly_mortgage['Rent_Income'] = ((1-marginal_tax_rate) * (1 - percent_vacancy) * 12 * monthly_rent * rent_percent ) * ((1 + rent_yearly_increase) ** yearly_mortgage['Year']) # Rent is taxed as income
         yearly_mortgage['Cumulative_Rent_Income'] = yearly_mortgage['Rent_Income'].cumsum()
 
         # Summary
@@ -137,12 +134,10 @@ class House():
         yearly_mortgage['Initial_Opportunity'] = ((down_payment+closing_buy)*offer) * (1 + investment_return)**(yearly_mortgage['Year']) * (1-long_term_capital_gains) # investing the downpayment and the closing cost of buying
         yearly_mortgage['Yearly_Rent_Cost'] = 12 * opportunity_rent * ((1 + rent_yearly_increase) ** yearly_mortgage['Year']) # The cost of renting
 
-        # Leftover money to invest if you choose to rent instead of buy, can be negative which means renting has opportunity cost
+        # Leftover money to invest if you choose to rent instead of buy, can be negative which means renting is more expensive than buying on a month-to-month basis
         yearly_mortgage['Extra_Investing_Money'] = p * 12+ yearly_mortgage['HOA']+ yearly_mortgage['Property_Tax']+ yearly_mortgage['Homeowner_Insurance']+ yearly_mortgage['Maintenance']- yearly_mortgage['Rent_Income']- yearly_mortgage['Effective_Tax_Savings']- yearly_mortgage['Yearly_Rent_Cost']
 
-        
-
-
+        #Invest the extra money saved by renting instead of buying
         yearly_growth = np.array([])
         for year in range(1,1 + time): #Take the extra investing money each year and invest it
             growth_years = yearly_mortgage[yearly_mortgage.Year <= year][['Year','Extra_Investing_Money']]
